fd43380fa208c2ca8c45addd9386b10639c2afe8
IbrahimAlispahic <ialispahic1@etf.unsa.ba>
2020-03-28 21:09:04 +0100

    server.js file changed.

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 4200);
 
 //PathLocationStrategy
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/LUDO-angular/src/index.html'));
 })
 
 console.log('Console listening!');
