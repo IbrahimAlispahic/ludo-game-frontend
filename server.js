d58f2ad93cd8118037d491c74f36f0dbecb5e06c
IbrahimAlispahic <ialispahic1@etf.unsa.ba>
2020-03-28 20:43:26 +0100

    server.js file changed.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.listen(process.env.PORT || 8080);
 //PathLocationStrategy
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('./index.html'));
+    res.sendFile('./index.html');
 })
 
 console.log('Console listening!');
